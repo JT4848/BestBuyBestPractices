c7d61a70d048b0cc507a491e064b22874bc12e31
John Thomas Pricone <jtpricone@gmail.com>
2023-01-11 18:32:42 -0500

    done

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BestBuyBestPractices;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+
+//var departmentRepo = new DapperDepartmentRepository(conn);
+
+//departmentRepo.InsertDepartment("My New Department");
+
+//var departments = departmentRepo.GetAllDepartments();
+
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//}
+
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.StockLevel);
+}
